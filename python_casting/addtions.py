dd2310d238f390b589e502646a87e5849c8ce314
Brijesh1990 <bkpandey.pandey@gmail.com>
2020-07-15 22:09:59 -0700

    Add files via upload

---
+++ python_casting/addtions.py
@@ -0,0 +1,9 @@
+#input() method that will used to take input from user
+
+a=int(input("Enter a Values"))
+
+b=int(input("Enter b Values"))
+
+c=a+b
+
+print("Addtions of two numers are :",c)
---
+++ python_casting/calc.py
@@ -0,0 +1,4 @@
+#w.ap to print additions choice based 1 additions 2 input multiplication 3 input divisions
+
+#python keywords
+
